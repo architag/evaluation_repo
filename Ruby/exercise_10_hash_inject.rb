2f28ff5d8b392171c0c9e7944083b737f77c3b72
Loconav <loconav@Loconavs-MacBook-Pro.local>
2021-05-13 13:02:09 +0530

    Implementation of 9,10 Exercise

---
+++ Ruby/exercise_10_hash_inject.rb
@@ -0,0 +1,32 @@
+class Array
+  def createHash
+    occurence = Hash.new { |hash, key| hash[key] = []}
+    self.each { |var| occurence[var.length] << var }
+    occurence
+  end
+end
+
+def hashInject(occurence)
+  groupHash = occurence.inject({}) do |mem, (key,val)|
+    if key%2 == 1
+      mem["odd"] ||= []
+      mem["odd"] << val
+    else
+      mem["even"] ||= []
+      mem["even"] << val
+    end
+    mem
+  end
+end
+
+input = ARGV[0].dup
+arr = Array.new()
+
+input.gsub!(/\[|\]/, "")
+input.split(',').each do |var|
+  var.gsub!(/"|'/, "")
+  arr << var
+end
+
+output = hashInject(arr.createHash())
+puts output
