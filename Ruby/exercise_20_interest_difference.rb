e5d11ee925f46a57b393e97ff8779f0c5ab02b0d
Loconav <loconav@Loconavs-MacBook-Pro.local>
2021-05-18 08:15:55 +0530

    Edited Exercise 20

--- Ruby/exercise_20_interest_difference.rb
+++ Ruby/exercise_20_interest_difference.rb
@@ -1,12 +1,14 @@
 class Interest
   attr_accessor :principal, :time
+
   def initialize
     yield self
   end
+
   def calc_difference
     rate = 0.1
-    comp = (principal*(1+rate)**time)
-    si = (principal*(1+rate*time))
+    comp = (@principal*(1+rate)**@time)
+    si = (@principal*(1+rate*@time))
     diff = (comp - si).round(2)
     diff
   end
@@ -15,7 +17,6 @@ end
 puts "Please provide an input"
 input = gets
 pr, t = input.split(" ").map { |var| var.to_f }
-
 obj = Interest.new do |var|
   var.principal = pr
   var.time = t
