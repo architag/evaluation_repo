ef676f49f553b461908f056f6f1457b643a296cf
Loconav <loconav@Loconavs-MacBook-Pro.local>
2021-07-07 17:42:38 +0530

    added support for user image upload and profile view

--- Rails/environment/lottery_ticketing_system/app/controllers/registrations_controller.rb
+++ Rails/environment/lottery_ticketing_system/app/controllers/registrations_controller.rb
@@ -2,5 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
   def create
     super
     resource.image = Image.create
+    resource.image.image_file.attach(params[:customer][:image][:image_file])
   end
 end
