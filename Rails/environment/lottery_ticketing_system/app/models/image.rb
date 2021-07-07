dd99f23f6d510857a5135f1856e43082217d9763
Loconav <loconav@Loconavs-MacBook-Pro.local>
2021-07-07 22:26:18 +0530

    customer image resize when displaying

--- Rails/environment/lottery_ticketing_system/app/models/image.rb
+++ Rails/environment/lottery_ticketing_system/app/models/image.rb
@@ -1,4 +1,9 @@
 class Image < ApplicationRecord
   belongs_to :imageable, polymorphic: true
   has_one_attached :image_file
+  validates :image_file, size: { less_than: 5.megabytes, message: "should be less than 5MB" }
+
+  def customer_image 
+    image_file.variant(resize_to_limit: [200, 200])
+  end
 end
